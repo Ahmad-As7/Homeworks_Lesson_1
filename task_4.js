8489f7c3d23e4ff183888cf722942442ad603aa7
Ahmad07777 <ahmadsooroosh@icloud.com>
2022-05-13 18:20:34 +0500

    first commit

---
+++ task_4.js
@@ -0,0 +1,9 @@
+// n second passed create time clock
+let n = 3602;
+let h = n/3600;
+let m = h / 60;
+let s = n % 3600
+console.log(`${Math.floor(h)} : ${Math.floor(m)}0 : ${Math.floor(s)}`)
+console.log(12)
+
+// 129700 ?
