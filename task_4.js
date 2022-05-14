68b337eb8f5f9582b62c1f507a1a2dd3d8bef445
Ahmad07777 <ahmadsooroosh@icloud.com>
2022-05-14 13:43:32 +0500

    second commit

--- task_4.js
+++ task_4.js
@@ -1,9 +1,12 @@
 // n second passed create time clock
-let n = 3602;
-let h = n/3600;
-let m = h / 60;
-let s = n % 3600
-console.log(`${Math.floor(h)} : ${Math.floor(m)}0 : ${Math.floor(s)}`)
-console.log(12)
-
-// 129700 ?
+function addZero (n) {
+    return n < 10 ? "0"+n : n
+}
+function getTime(n) { 
+let h = n/3600%24;
+let m = Math.floor(n%3600/60);
+let s = Math.floor(n % 3600 %60) 
+// let s1 = s < 10 ? "0"+s:s
+// let m1 = m < 10 ? "0"+m : m
+console.log(`${Math.floor(h)} : ${addZero(m)} : ${addZero(s)}`)
+}
